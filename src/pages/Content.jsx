7ee46917576c5d6f030d0cd2cf61b7d41003b101
W.Serm <wasan.serm019@gmail.com>
2025-06-19 06:01:59 +0700

    Update Content.jsx

--- src/pages/Content.jsx
+++ src/pages/Content.jsx
@@ -3,7 +3,7 @@ import TaskList from '../components/TaskList'
 export default function Content() {
   return (
     <section className='Content flex flex-1 items-stretch justify-center bg-gray-100'>
-      <div className="container max-w-full w-[85%] m-10 content-center">
+      <div className="container max-w-full w-[85%] content-center">
         <div className='flex flex-col self-stretch mx-auto px-8 py-5 gap-y-4 text-gray-800'>
           <TaskList/>
         </div>
