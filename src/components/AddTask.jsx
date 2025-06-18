272e64b7969833d45616ff1a17ccd3c32287782b
W.Serm <wasan.serm019@gmail.com>
2025-06-19 03:20:23 +0700

    Update AddTask.jsx

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -3,7 +3,7 @@ export default function AddTask({ task, setTask, addTask }) {
   return (
     <section className='CreateTask'>
       <form onSubmit={(e) => {addTask(task); e.preventDefault(); setTask('')}}>
-        <h6 className='text-base mt-2 mb-1'>Crate New Task</h6>
+        <h6 className='text-base sm:text-lg mt-2 mb-1'>Crate New Task</h6>
         <div className='flex flex-row text-base'>
           <input 
             type='text' 
@@ -16,7 +16,7 @@ export default function AddTask({ task, setTask, addTask }) {
           />
           <button 
             type='submit' 
-            className='w-28 py-1.5 font-semibold rounded-r-md border border-slate-600 text-white bg-slate-600 hover:bg-slate-700 cursor-pointer'
+            className='w-28 py-1.5 text-base sm:text-lg font-semibold rounded-r-md border border-slate-600 text-white bg-slate-600 hover:bg-slate-700 cursor-pointer'
             title='Add task'
           >
             ADD
