12487d267cb681fba11f7121cd99f3bb4e5080bf
W.Serm <wasan.serm019@gmail.com>
2025-06-19 03:22:06 +0700

    Update Counter.jsx

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,15 +1,14 @@
 export default function Counter({ tasksActive, tasksTotal, openModalDialog }) {
-  // const {tasksActive, tasksTotal, openModal} = prop;
   return (
     <section className='CountTask'>
       <div className='flex flex-col gap-y-2 sm:flex-row justify-between items-center'>
-        <span className='font-normal text-base'>
+        <span className='font-normal text-base sm:text-lg'>
           Active : {tasksActive} out of {tasksTotal} tasks
         </span>
-        <button 
-          type='button' 
+        <button
+          type='button'
           onClick={() => openModalDialog(false, 'reset')}
-          className='px-8 h-6.5 font-medium text-sm rounded-md shadow-sm outline outline-black/20 text-gray-600 bg-gray-50 hover:bg-gray-200' 
+          className='px-8 h-6.5 font-medium text-sm sm:text-base rounded-md shadow-sm outline outline-black/20 text-gray-600 bg-gray-50 hover:bg-gray-200'
           title='Reset'
         >
           Reset
