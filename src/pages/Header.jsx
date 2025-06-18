59c48c9c2faf2821342359b0b1a328e87765f37d
W.Serm <wasan.serm019@gmail.com>
2025-06-19 06:10:56 +0700

    Update Header.jsx

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -6,10 +6,10 @@ export default function Header() {
     <>
     <section className='Header'>
       <div className='flex flex-row px-6 sm:px-10 py-5 sm:py-6 gap-x-1 bg-slate-600 items-center'>
-        <img className='h-[28px] sm:h-[34px] object-cover' src={viteLogo} alt='vite logo'/>
+        <img className='h-[28px] sm:h-[36px] object-cover' src={viteLogo} alt='vite logo'/>
         <h1 className='font-poppins font-bold text-white text-2xl sm:text-5xl'>Vite</h1>
         <h1 className='ml-0.5 mr-0.5 font-poppins font-bold text-white text-2xl sm:text-5xl'>+</h1>
-        <img className='h-[28px] sm:h-[34px] object-cover' src={reactLogo} alt='react logo'/>
+        <img className='h-[28px] sm:h-[36px] object-cover' src={reactLogo} alt='react logo'/>
         <h1 className='font-poppins font-bold text-white text-2xl sm:text-5xl'>React</h1>
         <h1 className='ml-1 font-poppins font-bold text-white text-2xl sm:text-5xl'>: Task List</h1>
       </div>
