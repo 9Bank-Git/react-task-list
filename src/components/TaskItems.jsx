36d97dc8f28a73aa6d1b863ad284b6d4e1f45de1
W.Serm <wasan.serm019@gmail.com>
2025-06-19 03:27:51 +0700

    Update TaskItems.jsx

--- src/components/TaskItems.jsx
+++ src/components/TaskItems.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react';
 
 export default function TaskItems({ displayTasks, showActive, setShowActive, toggleCompleted, moveTaskUp, moveTaskDown, editedTask, openModalDialog }) {
-  // const { displayTasks, showActive, setShowActive, toggleCompleted, moveTaskUp, moveTaskDown, editedTask, openModal } = props
   return (
     <div className='task-list flex flex-col'>
       <div className='relative flex'>
-        <p className='flex items-center text-lg mt-2 mb-1'>Task List</p>
+        <p className='flex items-center text-lg sm:text-xl mt-2 mb-1'>Task List</p>
         <div className='absolute top-3.5 left-25 flex items-center absolute'>
           <input type='checkbox' checked={showActive} onChange={(e) => setShowActive(e.target.checked)} className='accent-sky-600'/>
-          <span className='pl-1 text-sm text-gray-700'>Show only active</span>
+          <span className='pl-1 text-sm sm:text-base text-gray-700'>Show only active</span>
         </div>
       </div>
       <hr className='text-slate-300 mb-3'/>
@@ -44,35 +43,35 @@ function ItemsList({ task, index, completedTask, moveTaskUp, moveTaskDown, edite
     <li className='flex flex-col sm:flex-row px-2 py-3 gap-y-2 sm:gap-x-2 rounded shadow-sm bg-gray-100 items-center'>
       <div className='flex pl-2 gap-x-4 items-center'>
         <input type='checkbox' checked={task.completed} onChange={() => completedTask(task.id)} className='size-4 accent-gray-500/25 hover:opacity-60 items-center'/>
-        <span className={`size-5 rounded-full font-semibold text-sm text-center content-center ${task.completed ? 'text-gray-100 bg-sky-200' : 'text-white bg-sky-500'}`}>
+        <span className={`size-5 rounded-full font-semibold text-sm sm:text-base text-center content-center ${task.completed ? 'text-gray-100 bg-sky-200' : 'text-white bg-sky-500'}`}>
           {index + 1}
         </span>
       </div>
       {isEditing ? (
         <input type='text' value={editText} onChange={(e) => setEditText(e.target.value)} className='grow text-base rounded border border-gray-400 focus:outline-none'/>
       ) : (
-        <span className={`flex-1 font-light text-lg text-center sm:text-left break-all ${task.completed ? 'line-through text-gray-400' : 'text-gray-700'}`}>{task.text}</span>
+        <span className={`flex-1 font-light text-lg sm:text-xl text-center sm:text-left break-all ${task.completed ? 'line-through text-gray-400' : 'text-gray-700'}`}>{task.text}</span>
       )
       }
       {isEditing ? (
         <div className='actions flex space-x-1'>
           <button onClick={() => moveTaskUp(index)} className='flex px-1.5 py-1 rounded text-gray-500 hover:bg-zinc-200 items-center'>
-            <i className='material-icons' style={{fontSize: '18px'}}>arrow_upward</i>
+            <i className='material-icons' style={{fontSize: '20px'}}>arrow_upward</i>
           </button>
           <button onClick={() => moveTaskDown(index)} className='flex px-1.5 py-1 rounded text-gray-500 hover:bg-zinc-200 items-center'>
-            <i className='material-icons' style={{fontSize: '18px'}}>arrow_downward</i>
+            <i className='material-icons' style={{fontSize: '20px'}}>arrow_downward</i>
           </button>
           <button onClick={() => handleSave(task.id)} className='flex px-1.5 py-1 rounded text-gray-500 hover:bg-zinc-200 items-center'>
-            <i className='material-icons' style={{fontSize: '18px'}}>save</i>
+            <i className='material-icons' style={{fontSize: '20px'}}>save</i>
           </button>
         </div>
       ) : (
         <div className='actions flex space-x-2'>
           <button onClick={() => handleEdit(index)} className='flex px-1.5 py-1 rounded text-gray-500 hover:bg-zinc-200 items-center'>
-            <i className='material-icons' style={{fontSize: '18px'}}>create</i>
+            <i className='material-icons' style={{fontSize: '20px'}}>create</i>
           </button>
           <button onClick={() => openModalDialog(true, task.id)} className='flex px-1.5 py-1 rounded text-gray-500 hover:bg-zinc-200 items-center'>
-            <i className='material-icons' style={{fontSize: '18px'}}>delete</i>
+            <i className='material-icons' style={{fontSize: '20px'}}>delete</i>
           </button>
         </div>
       )}          
