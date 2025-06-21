6b545e1f6dfede54e3109311fe4eac10fe4a4189
Wasan Sermsirisophon <wasan.serm019@gmail.com>
2025-06-21 19:16:00 +0700

    Update TaskItems.jsx

--- src/components/TaskItems.jsx
+++ src/components/TaskItems.jsx
@@ -3,22 +3,24 @@ import { toast } from 'react-toastify'
 
 export default function TaskItems({ displayTasks, showActive, setShowActive, toggleCompleted, moveTaskUp, moveTaskDown, editedTask, openModalDialog }) {
   return (
-    <div className='task-list flex flex-col'>
-      <div className='relative flex mt-2 mb-1'>
-        <p className='text-base sm:text-lg'>Tasks List</p>
-        <label className='absolute top-1 left-25 flex items-center'>
-          <input type='checkbox' checked={showActive} onChange={(e) => setShowActive(e.target.checked)} className='accent-sky-600'/>
-          <span className='pl-1 text-sm sm:text-base text-gray-700'>Show only active</span>
-        </label>
+    <section className='TaskList'>
+      <div className='flex flex-col'>
+        <div className='relative flex mt-2 mb-1'>
+          <p className='text-base sm:text-lg'>Tasks List</p>
+          <label className='absolute top-1 left-25 sm:left-30 flex items-center'>
+            <input type='checkbox' checked={showActive} onChange={(e) => setShowActive(e.target.checked)} className='accent-sky-600'/>
+            <span className='pl-1 text-sm sm:text-base text-gray-700'>Show only active</span>
+          </label>
+        </div>
+        <hr className='text-slate-300 mb-3'/>
+        <ul className='flex flex-col gap-y-2 min-h-[380px] sm:min-h-[420px]'>
+          {displayTasks.map((task, index) => (
+            <ItemsList key={task.id} task={task} index={index} completedTask={toggleCompleted} moveTaskUp={moveTaskUp} moveTaskDown={moveTaskDown} editedTask={editedTask} openModalDialog={openModalDialog}/>
+          ))}
+        </ul>
+        <hr className='text-slate-300 mt-3'/>
       </div>
-      <hr className='text-slate-300 mb-3'/>
-      <ul className='tasks-content flex flex-col gap-y-2 min-h-[380px] sm:min-h-[420px]'>
-        {displayTasks.map((task, index) => (
-          <ItemsList key={task.id} task={task} index={index} completedTask={toggleCompleted} moveTaskUp={moveTaskUp} moveTaskDown={moveTaskDown} editedTask={editedTask} openModalDialog={openModalDialog}/>
-        ))}
-      </ul>
-      <hr className='text-slate-300 mt-3'/>
-    </div>
+    </section>
   );
 }
 
@@ -42,7 +44,7 @@ function ItemsList({ task, index, completedTask, moveTaskUp, moveTaskDown, edite
   }
 
   return (
-    <li className='flex flex-col sm:flex-row px-2 py-3 gap-y-2 sm:gap-x-2 rounded shadow-sm bg-white items-center'>
+    <li className='flex flex-col sm:flex-row px-2 py-3 gap-y-2 sm:gap-x-2 rounded shadow bg-white items-center'>
       <div className='flex pl-2 gap-x-4 items-center'>
         <input type='checkbox' checked={task.completed} onChange={() => completedTask(task.id)} className='size-4 accent-gray-500/25 hover:opacity-60 items-center'/>
         <span className={`size-5 rounded-full font-semibold text-sm sm:text-base text-center content-center ${task.completed ? 'text-gray-100 bg-sky-200' : 'text-white bg-sky-500'}`}>
@@ -56,28 +58,28 @@ function ItemsList({ task, index, completedTask, moveTaskUp, moveTaskDown, edite
           <span className={`${task.completed ? 'line-through text-gray-400' : 'text-gray-700'}`}>{task.text}</span>
         )}
       </div>
-      <div className='flex mr-1 space-x-1.5'>
+      <div className='flex mr-1 space-x-2'>
         {isEditing ? (
           <>
-          <button title='Move up' onClick={() => moveTaskUp(index)} className='flex px-1.5 py-1 rounded text-gray-500 hover:bg-zinc-200 items-center'>
+          <button type='button' title='Move up' onClick={() => moveTaskUp(index)} className='flex px-1.5 py-1 rounded text-gray-500 hover:bg-zinc-200 items-center'>
             <i className='material-icons' style={{fontSize: '20px'}}>arrow_upward</i>
           </button>
-          <button title='Move down' onClick={() => moveTaskDown(index)} className='flex px-1.5 py-1 rounded text-gray-500 hover:bg-zinc-200 items-center'>
+          <button type='button' title='Move down' onClick={() => moveTaskDown(index)} className='flex px-1.5 py-1 rounded text-gray-500 hover:bg-zinc-200 items-center'>
             <i className='material-icons' style={{fontSize: '20px'}}>arrow_downward</i>
           </button>
-          <button title='Save' onClick={() => handleSave(task.id)} className='flex px-1.5 py-1 rounded text-gray-500 hover:bg-zinc-200 items-center'>
+          <button type='button' title='Save' onClick={() => handleSave(task.id)} className='flex px-1.5 py-1 rounded text-gray-500 hover:bg-zinc-200 items-center'>
             <i className='material-icons' style={{fontSize: '20px'}}>save</i>
           </button>
-          <button title='Cancel' onClick={() => setIsEditing(false)} className='flex px-1.5 py-1 rounded text-gray-500 hover:bg-zinc-200 items-center'>
+          <button type='button' title='Cancel' onClick={() => setIsEditing(false)} className='flex px-1.5 py-1 rounded text-gray-500 hover:bg-zinc-200 items-center'>
             <i className='material-icons' style={{fontSize: '20px'}}>close</i>
           </button>
           </>
         ) : (
           <>
-          <button title='Edit' onClick={() => handleEdit(index)} className='flex px-1.5 py-1 rounded text-gray-500 hover:bg-zinc-200 items-center'>
+          <button type='button' title='Edit' onClick={() => handleEdit(index)} className='flex px-1.5 py-1 rounded text-gray-500 hover:bg-zinc-200 items-center'>
             <i className='material-icons' style={{fontSize: '20px'}}>edit</i>
           </button>
-          <button title='Delete' onClick={() => openModalDialog(true, task.id)} className='flex px-1.5 py-1 rounded text-gray-500 hover:bg-zinc-200 items-center'>
+          <button type='button' title='Delete' onClick={() => openModalDialog(true, task.id)} className='flex px-1.5 py-1 rounded text-gray-500 hover:bg-zinc-200 items-center'>
             <i className='material-icons' style={{fontSize: '20px'}}>delete</i>
           </button>
           </>
