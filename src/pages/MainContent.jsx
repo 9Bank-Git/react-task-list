92f05f2fbb8fcc1fb2d4429d3d145a52997c225b
Wasan Sermsirisophon <wasan.serm019@gmail.com>
2025-06-21 22:17:23 +0700

    Update MainContent.jsx

--- src/pages/MainContent.jsx
+++ src/pages/MainContent.jsx
@@ -4,7 +4,7 @@ export default function MainContent() {
   return (
     <section className='MainContent'>
         <div className='container max-w-full w-[93%] justify-center content-center'> 
-          <div className='flex flex-1 px-8 py-6 gap-y-4 text-gray-800'>
+          <div className='flex flex-col flex-1 px-8 py-6 gap-y-4 text-gray-800'>
             <TaskList/>
           </div>
         </div>
