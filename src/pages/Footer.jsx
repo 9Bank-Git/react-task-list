2f70cee846bb0435385631a7781de5f79130dd30
Wasan Sermsirisophon <wasan.serm019@gmail.com>
2025-06-21 15:41:34 +0700

    Update Footer.jsx

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <section className='Footer'>
       <div className='flex flex-col sm:flex-row px-4 sm:px-2 py-4 gap-x-2 text-base sm:text-lg text-white bg-slate-600 items-center justify-center'>
         <span>&#169; { `${new Date().getFullYear()} TailwindCSS Theme - Made By`}</span>
-        <a href='https://github.com/9Bank-Git' target='_blank' title='Visit GitHub' className='pl-2 text-cyan-300 no-underline hover:underline'>9BANK-GitHub</a>
+        <a href='https://github.com/9Bank-Git' target='_blank' title='Visit GitHub' className='pl-1 text-cyan-300 no-underline hover:underline'>9Bank-Git</a>
       </div>
     </section>
   )
