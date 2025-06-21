f93f6947fa17c809fb3baa1dbc004c1ecad22abb
Wasan Sermsirisophon <wasan.serm019@gmail.com>
2025-06-21 21:35:56 +0700

    Update MainContent.jsx

--- src/pages/MainContent.jsx
+++ src/pages/MainContent.jsx
@@ -3,11 +3,9 @@ import TaskList from '../components/TaskList'
 export default function MainContent() {
   return (
     <section className='MainContent'>
-      <div className='flex flex-1 items-stretch justify-center bg-gray-100'>
-        <div className="container max-w-full w-[93%] content-center">
-          <div className='flex flex-col self-stretch mx-auto px-8 py-5 gap-y-4 text-gray-800'>
-            <TaskList/>
-          </div>
+      <div className="container max-w-full w-[93%] justify-center place-items-center bg-gray-100"> 
+        <div className='flex flex-col flex-1 mx-auto px-8 py-5 gap-y-4 text-gray-800'>
+          <TaskList/>
         </div>
       </div>
     </section>
